1d3a5ce24119e0752cb483819415e104f190e7e6
Kiraprint <76822921+Kiraprint@users.noreply.github.com>
2021-01-06 22:51:57 +0300

    coffee viewer v. 1.2

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sqlite3
 import sys
 
-from PyQt5 import uic, QtCore, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QWidget
 
 
 def except_hook(cls, exception, traceback):
@@ -15,103 +15,120 @@ class MyWidget(QMainWindow):
         uic.loadUi('main.ui', self)
 
         self.connect = sqlite3.connect('coffee_db.sqlite')
-        '''
-        self.add_pack.clicked.connect(self.add_f)
-        self.add_sort.clicked.connect(self.add_g)
-        self.delete_pack.clicked.connect(self.delete_film)
-        self.delete_sort.clicked.connect(self.delete_genre)
-        self.edit_pack.clicked.connect(self.edit_f)
-        self.edit_sort.clicked.connect(self.edit_g)
-        '''
-        self.coffee_table.setColumnCount(7)
-        self.sorts_table.setColumnCount(2)
-        self.sorts_table.setHorizontalHeaderLabels(['ID', 'Sort name'])
+        self.coffee_table.setColumnCount(8)
         self.coffee_table.setHorizontalHeaderLabels(['ID', 'Pack title',
-                                                   'Coffee sort', 'Degree of roast',
-                                                   'Volume of pack', 'Ground or whole', 'Taste'])
+                                                     'Degree of roast', 'Volume of pack', 'Ground or whole',
+                                                     'Taste', 'Coffee sort', 'Price'])
 
-        self.coffee_load()
-        self.sorts_load()
+        self.modified = {}
 
-    def coffee_load(self):
+        self.add_pb.clicked.connect(self.add)
+        self.edit_pb.clicked.connect(self.edit)
+
+        self.add_screen = AddCoffee()
+        self.load()
+
+    def load(self):
         cursor = self.connect.cursor()
-        res = cursor.execute("""select coffee.id, coffee.title,  sorts.name, degree_of_roast, coffee.volume_of_pack, 
-         ground, taste from coffee, sorts where sorts.id = coffee.sort_id""").fetchall()
+        res = cursor.execute("""select * from coffee""").fetchall()
         for i, info in enumerate(res):
             self.coffee_table.setRowCount(i + 1)
             for j, inf in enumerate(info):
                 self.coffee_table.setItem(i, j, QTableWidgetItem(str(inf)))
 
-    def sorts_load(self):
-        cursor = self.connect.cursor()
-        res = cursor.execute("""select * from sorts""").fetchall()
-        for i, info in enumerate(res):
-            self.sorts_table.setRowCount(i + 1)
-            for j, inf in enumerate(info):
-                self.sorts_table.setItem(i, j, QTableWidgetItem(str(inf)))
+    def add(self):
+        self.add_screen.show()
 
-    '''
-    self.add_film_action = AddPack()
-    self.add_genre_action = AddSort()
-    self.edit_film_action = EditPack()
-    self.edit_genre_action = EditSort()
-    
+    def edit(self):
+        ed_f = self.coffee_table.takeItem(self.coffee_table.currentRow(), 0).text()
+        self.edit_screen = EditCoffee(ed_f)
+        self.edit_screen.show()
 
-def delete_film(self):
-    cur = self.connect.cursor()
-    for i in self.film_table.selectedItems():
-        cur.execute("""delete from coffee where title = ?""", (i.text(),))
-    self.connect.commit()
-    self.coffee_load()
-
-def delete_genre(self):
-    cur = self.connect.cursor()
-    for i in self.genre_table.selectedItems():
-        cur.execute("""delete from sorts where name = ?""", (i.text(),))
-    self.connect.commit()
-    self.sorts_load()
-    self.coffee_load()
-
-def edit_f(self):
-    a = self.film_table.selectedItems()
-    if a:
-        cur = self.connect.cursor()
-        id_new, name, year, genre, length = map(str, cur.execute("""select * from coffee 
-        where title = ?""", (a[0].text(),)).fetchall()[0])
-        genres = list(map(lambda x: x[0], cur.execute("""select distinct name 
-        from sorts""").fetchall()))
-        self.edit_film_action.film_genre.addItems(genres)
-        self.edit_film_action.id = id_new
-        self.edit_film_action.film_name.setText(name)
-        self.edit_film_action.film_year.setText(year)
-        self.edit_film_action.film_length.setText(length)
-        self.edit_film_action.film_genre.setCurrentText(cur.execute("""select title 
-        from genres 
-        where id = ?""", (genre,)).fetchall()[0][0])
-        self.edit_film_action.show()
-
-def edit_g(self):
-    a = self.genre_table.selectedItems()
-    if a:
-        cur = self.connect.cursor()
-        id_new, name = map(str, cur.execute("""select * from genres where title = ?""",
-                                            (a[0].text(),)).fetchall()[0])
-        self.edit_genre_action.name_edit.setText(name)
-        self.edit_genre_action.id = id_new
-        self.edit_genre_action.show()
-
-def add_f(self):
-    cur = self.connect.cursor()
-    self.add_film_action.genres = list(map(lambda x: x[0], cur.execute("""select distinct title 
-                                    from genres""").fetchall()))
-    self.add_film_action.film_genre.addItems(self.add_film_action.genres)
-    self.add_film_action.show()
-
-def add_g(self):
-    self.add_genre_action.show()
+    def save_results(self):
+        if self.modified:
+            cur = self.con.cursor()
+            que = "UPDATE films SET\n"
+            for key in self.modified.keys():
+                que += "{}='{}'\n".format(key, self.modified.get(key))
+            que += "WHERE id = ?"
+            cur.execute(que, (self.spinBox.text(),))
+            self.con.commit()
+            self.modified.clear()
 
 
-'''
+class AddCoffee(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.confirm_pb.setText('Add')
+        self.confirm_pb.clicked.connect(self.add)
+
+        self.connect = sqlite3.connect('coffee_db.sqlite')
+
+    def add(self):
+        try:
+            title = self.title_le.text()
+            sort = self.sort_le.text()
+            dor = self.dor_cb.currentText()
+            vop = int(self.vop_sb.value())
+            gow = self.gow_bg.checkedButton().text()
+            taste = self.taste_le.text()
+            price = int(self.price_sb.value())
+
+            cur = self.connect.cursor()
+            new_id = cur.execute("""select max(id) from coffee""").fetchall()[0][0] + 1
+            cur.execute("""insert into coffee 
+                        values (?, ?, ?, ?, ?, ?, ?, ?)""",
+                        (new_id, title, dor, vop, gow, taste, sort, price))
+
+            self.connect.commit()
+            self.close()
+        except ValueError:
+            self.close()
+
+    def closeEvent(self, event):
+        self.title_le.clear()
+        self.sort_le.clear()
+        self.taste.clear()
+        form.load()
+
+
+class EditCoffee(QWidget):
+    def __init__(self, coid):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.confirm_pb.setText('Edit')
+        self.confirm_pb.clicked.connect(self.edit)
+
+        self.id = coid
+        self.con = sqlite3.connect('coffee_db.sqlite')
+        self.cur = self.con.cursor()
+        self.data = self.cur.execute("""select * from coffee where id=?""", (int(coid),)).fetchone()
+        self.title_le.setText(self.data[1])
+        self.sort_le.setText(self.data[6])
+
+    def edit(self):
+        try:
+            title = self.title_le.text()
+            sort = self.sort_le.text()
+            dor = self.dor_cb.currentText()
+            vop = int(self.vop_sb.value())
+            gow = self.gow_bg.checkedButton().text()
+            taste = self.taste_le.text()
+            price = int(self.price_sb.value())
+
+            self.cur.execute("""update coffee 
+            set title = ?, degree_of_roast = ?, volume_of_pack = ?, 
+            ground = ?, taste=?, sort=?, price=? where id = ?""",
+                             (title, dor, vop, gow,
+                              taste, sort, price, self.id))
+            self.con.commit()
+            self.close()
+        except ValueError:
+            self.close()
+
+    def closeEvent(self, event):
+        form.load()
 
 
 if __name__ == '__main__':
